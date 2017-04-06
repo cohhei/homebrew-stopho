e3ab6d326bd5fc49cb5d86c11f4d6075e20a419b
Ko <ko.kimur@gmail.com>
2017-04-06 10:03:55 +0900

    Update stopho.rb

--- stopho.rb
+++ stopho.rb
@@ -1,17 +1,16 @@
 require "formula"
 
-HOMEBREW_HTTR_VERSION='0.0.2'
+HOMEBREW_HTTR_VERSION='v0.1.0'
 
 class Httr < Formula
   if Hardware.is_64_bit?
-    url "https://github.com/kohei-kimura/stopho/releases/download/v0.1.0/stopho_darwin_386"
+    url "https://github.com/kohei-kimura/stopho/releases/download/#{HOMEBREW_HTTR_VERSION}/stopho_darwin_386"
     sha256 "8dfb0e60a6d6cea7719564e7754f54b0132a8bfdf4febca06521f4baa47dd4f2"
   else
-    url "https://github.com/kohei-kimura/stopho/releases/download/v0.1.0/stopho_darwin_amd64"
+    url "https://github.com/kohei-kimura/stopho/releases/download/#{HOMEBREW_HTTR_VERSION}/stopho_darwin_amd64"
     sha256 "146f2cbda8440c4e01f839d391aeea84fb16d9f3d9d4f94fc601c978885e8d2f"
   end
   homepage "https://github.com/kohei-kimura/stopho"
-  sha256 ""
 
   version HOMEBREW_HTTR_VERSION
   head 'https://github.com/kohei-kimura/stopho.git', :branch => 'master'
