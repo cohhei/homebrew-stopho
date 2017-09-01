621d298f01696a758571b1b162fc858ece45f658
Ko <ko.kimur@gmail.com>
2017-09-01 22:39:01 +0900

    fix sha256

--- stopho.rb
+++ stopho.rb
@@ -5,10 +5,10 @@ VERSION='v0.1.1'
 class Stopho < Formula
   if Hardware::CPU.is_64_bit?
     url "https://github.com/kohei-kimura/stopho/releases/download/#{VERSION}/stopho_darwin_386"
-    sha256 "f1e4c572467e26d2a83d34c76fe40836108dc52664919464a17a7319f74faf2a"
+    sha256 "d0aabb0885d3136cdcefcd0260b69989aa40f8c297939997f71df32d0fa2aace"
   else
     url "https://github.com/kohei-kimura/stopho/releases/download/#{VERSION}/stopho_darwin_amd64"
-    sha256 "29ffbd86f7aaf9912230e12e3a3bf9e298f0d04c6d1fcfcabf6638e1f2670ef4"
+    sha256 "8a4af554335350de17610d09763a7731fc261d0e07498ac5b7eea0db3354b628"
   end
   homepage "https://github.com/kohei-kimura/stopho"
 
