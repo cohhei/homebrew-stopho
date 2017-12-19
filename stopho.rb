e976cdd6fee0876376a1795d1bc6ebca9de1c344
Ko <ko.kimur@gmail.com>
2017-12-19 10:40:26 +0900

    Update to v0.2.0!

--- stopho.rb
+++ stopho.rb
@@ -1,14 +1,14 @@
 require "formula"
 
-VERSION='v0.1.1'
+VERSION='v0.2.0'
 
 class Stopho < Formula
   if Hardware::CPU.is_64_bit?
     url "https://github.com/kohei-kimura/stopho/releases/download/#{VERSION}/stopho_darwin_386"
-    sha256 "841a952bd37d68dc7d69efa7e4f9f685ea0257dc5ce6d0186c4d54ebd9f6c3ac"
+    sha256 "5173f60de47eba97fb4c3d3ab5a894f3f55c6d463c393bb4eeb54ab48ed72d64"
   else
     url "https://github.com/kohei-kimura/stopho/releases/download/#{VERSION}/stopho_darwin_amd64"
-    sha256 "cb35b2a0defcc27fadd8f344512953835f97e00e6e9f16689adb781d5ac44013"
+    sha256 "c8a78a2e13cda1417ba513a03cd33ea18bfd400bece86e58e067dd9f36bc4345"
   end
   homepage "https://github.com/kohei-kimura/stopho"
 
